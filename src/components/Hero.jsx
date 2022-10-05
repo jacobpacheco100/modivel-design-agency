71abee061f2cd878b45297fbdcd6ea13e57e8c07
Jacob Pacheco <72306809+JacobPacheco100@users.noreply.github.com>
2022-10-04 22:37:44 -0400

    added: hero wip

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import Spline from '@splinetool/react-spline'
 
 function Hero() {
-  return <div></div>
+  return (
+    <>
+      <main className='mt-[60px] h-[700px] flex justify-between items-center'>
+        <section className='txt-group-1'>
+          <h1 className='heading-1'>
+            Modivel <br />
+            Design <br />
+            Agency
+          </h1>
+          <p className='p1 max-w-[400px]'>
+            Welcome, we design and develope your brand from the ground up to
+            stand out among your competitors on the web.
+          </p>
+          <button className='btn-1'>Get Started</button>
+        </section>
+        <section className='grow h-full hidden desktop:block'>
+          <Spline scene='https://prod.spline.design/5YNozaAcQyWK21gO/scene.splinecode' />
+        </section>
+      </main>
+    </>
+  )
 }
 
 export default Hero
