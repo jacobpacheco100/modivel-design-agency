73bdb4072f6d04df0a0c224e4b683186211d8717
Jacob Pacheco <72306809+JacobPacheco100@users.noreply.github.com>
2022-10-09 14:02:25 -0400

    added: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import logo from '../assets/svg/logo-footer.svg'
 
+// backToTop
+import { MdArrowUpward } from 'react-icons/md'
+
 // media icons
 import { BsTwitter } from 'react-icons/bs'
 import { BsFacebook } from 'react-icons/bs'
@@ -9,29 +12,37 @@ import { BsSlack } from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <main className=' mt-40 bg-dark py-28'>
-      <div className='flex flex-col items-center space-y-14'>
-        <div className='flex flex-col items-center'>
+    <main className=' mt-40 bg-[#131432] py-28 desktop:py-10'>
+      {/* footer content */}
+      <div className='container flex flex-col items-center space-y-14 desktop:space-y-0 desktop:flex-row desktop:space-x-20 desktop:justify-start relative'>
+        {/* logo and about */}
+        <div className='flex flex-col items-center desktop:flex-row desktop:space-x-10'>
           <img src={logo} alt='logo' />
-          <p className='text-[14px] text-[#646284] font-medium text-center max-w-[225px] mt-7'>
-            Are you a designer, developer, or software developer? If your
-            looking for a hiring opportuity, click the link.
+          <p className='text-[14px] text-[#646284] font-medium text-center max-w-[225px] mt-7 desktop:max-w-[300px] desktop:mt-0 desktop:text-left'>
+            We are a proud startup design agency focused on making beutifull and
+            intelligent design desicions for our clients
           </p>
         </div>
-        <div className='text-blueGray text-3xl flex justify-between w-[190px]'>
-          <BsTwitter />
-          <BsFacebook />
-          <BsLinkedin />
-          <BsSlack />
+        {/* media links */}
+        <div className='text-blueGray text-3xl desktop:text-xl flex justify-between w-[190px]'>
+          <BsTwitter className='hover:cursor-pointer' />
+          <BsFacebook className='hover:cursor-pointer' />
+          <BsLinkedin className='hover:cursor-pointer' />
+          <BsSlack className='hover:cursor-pointer' />
         </div>
+        {/* contact */}
         <div className='flex flex-col items-center'>
-          <p className='p font-medium text-darkGray text-[14px]'>
+          <p className=' font-medium text-darkGray text-[14px]'>
             813 - 989 -7853
           </p>
-          <p className='p font-medium text-darkGray text-[14px]'>
+          <p className=' font-medium text-darkGray text-[14px]'>
             reachmodivel@gmail.com
           </p>
         </div>
+        {/* back to top */}
+        <button className='back-to-top '>
+          <MdArrowUpward />
+        </button>
       </div>
     </main>
   )
